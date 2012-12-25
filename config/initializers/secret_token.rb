16d2705329f2f1f58e5bc8c7eb4a45a3c4f0b691
basile bouquet <bas.bouquet@gmail.com>
2012-12-25 05:08:02 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Essai::Application.config.secret_token = '24f6a33389b34983f8fb2596db70aeaacd3a1eb6d0240ff1bac111cd271330defba1d8aa1ad6d017a10a8c17340ef10391d4ccb182d92ea942b17ac391fb4dac'
